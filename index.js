52120b20f03fe43854974ab1cee003555fac41b5
convivial-reducer-7231 <tradford10@gmail.com>
2020-10-03 20:56:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = ''${conversation}'' 'space';
+var topic = 'space, ${conversation}';
 
 var conversation = `${topic}`;
